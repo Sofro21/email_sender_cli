35d0925853c0fb854903ce20a1c3454caae6a6a1
sofro21 <andreisofronie25@gmail.com>
2024-03-26 15:17:50 +0100

    create-basic-UI

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import smtplib
+from tkinter.simpledialog import askstring
+from tkinter import *
 
-def automatic_email_sender(email_address):
+def automatic_email_sender(email_address, message1):
     if type(email_address) is not str:
         raise Exception("Wrong Input")
     smtp_server = 'smtp.gmail.com'
@@ -11,7 +13,7 @@ def automatic_email_sender(email_address):
     from_email = 'andreisofroniesubscriptions@gmail.com'
     to_email = email_address
     subject = 'My first automatic email!'
-    body = 'This is a test email.'
+    body = message1
 
     message = f'Subject: {subject}\n\n{body}'
     with smtplib.SMTP(smtp_server, smtp_port) as smtp:
@@ -19,6 +21,15 @@ def automatic_email_sender(email_address):
         smtp.login(smtp_username, smtp_password)
         smtp.sendmail(from_email, to_email, message)
 
-            
-email_address = 'randomnesstest2@gmail.com'
-automatic_email_sender(email_address)
+
+top = Tk()
+
+top.geometry("1000x1000")
+def show():
+   message = askstring("Input", "Enter the message you want to send")
+   automatic_email_sender('andreisofronie25@gmail.com', message)
+   
+B = Button(top, text ="Click", command = show)
+B.place(x=500,y=500)
+
+top.mainloop()
