ea3997b068ea59ef2e6a99b5514a4a5a303db2dd
sofro21 <andreisofronie25@gmail.com>
2024-03-29 20:50:11 +0200

    automatically replace a specific place with user input info

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import smtplib
 from tkinter.simpledialog import askstring
 from tkinter import *
 
-def automatic_email_sender(email_address, message1):
-    if type(email_address) is not str:
-        raise Exception("Wrong Input")
+def automatic_email_sender(email_address, message1, user_input):
+    email = email_address.get()
+    ui = user_input.get()
+    message2 = message1.get("1.0", "end-1c")
+    message2 = message2.replace("{DEMIR}", user_input.get())
+
+
     smtp_server = 'smtp.gmail.com'
     smtp_port = 587
     smtp_username = 'andreisofroniesubscriptions@gmail.com'
     smtp_password = 'zoxmvjqjwodtsjhr'
 
     from_email = 'andreisofroniesubscriptions@gmail.com'
-    to_email = email_address
+    to_email = email
     subject = 'My first automatic email!'
-    body = message1
+    body = message2
 
     message = f'Subject: {subject}\n\n{body}'
     with smtplib.SMTP(smtp_server, smtp_port) as smtp:
@@ -25,23 +29,17 @@ def automatic_email_sender(email_address, message1):
 top = Tk()
 top.geometry("1000x1000")
 
-
-def show():
-   email = askstring("Input", "Enter the receiver e-mail address")
-   message = askstring("Input", "Enter the message you want to send")
-   automatic_email_sender(email, message)
-
-
-
-# B = Button(top, text ="Click", command = show)
-# B.place(x=500,y=500)
-
-
 email_label = Label(top, text="Email Address:")
 email_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
 email_entry = Entry(top, width=40)
 email_entry.grid(row=0, column=1, padx=5, pady=5)
+
+random_user_input_label = Label(top, text="Random user input:")
+random_user_input_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+
+random_user_input = Entry(top, width=40)
+random_user_input.grid(row=2, column=1, padx=5, pady=5)
 
 message_label = Label(top, text="Message:")
 message_label.grid(row=1, column=0, padx=5, pady=5, sticky="nw")
@@ -49,7 +47,7 @@ message_label.grid(row=1, column=0, padx=5, pady=5, sticky="nw")
 message_text = Text(top, width=40, height=10)
 message_text.grid(row=1, column=1, padx=5, pady=5, sticky="nw")
 
-send_button = Button(top, text="Send Email")
+send_button = Button(top, text="Send Email", command=lambda: automatic_email_sender(email_entry,message_text,random_user_input))
 send_button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
 
 
